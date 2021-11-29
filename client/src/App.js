81c1d9d1c5f01dc25e3bb74ffcd0405e3c17d11f
AnnapurnaAnanya <90723139+AnnapurnaAnanya@users.noreply.github.com>
2021-11-29 04:13:06 -0800

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/Main.js'
+
+function App() {
+  return (
+   
+      <div>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
